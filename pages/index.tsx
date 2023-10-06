ca1e6dea10e421c015a4240764aa6198d7284ad1
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2023-10-06 12:54:07 -0600

    Update Fetch URL to stable

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const myInit = {
 
 async function GeneratorHandler (req:NextApiRequest,res:NextApiResponse<GenText>) {
   
- var data = await fetch(`https://${process.env.CF_PAGES_URL}.collate.pages.dev/api/gptSummary` , myInit)
+ var data = await fetch(`https://collate.community/api/gptSummary` , myInit)
   
   return console.log(data )
 

e2cda0bbbc712f3e953eef7958c188603f31b9a2
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2023-10-19 10:22:40 -0600

    edge runtime push, will break

--- pages/api/gptSummary.ts
+++ pages/api/gptSummary.ts
@@ -11,7 +11,17 @@ interface GenText {
   Text:string | null
 }
 
-
+// import { NextResponse } from 'next/server'
+// import squareWasm from './square.wasm?module'
+ 
+// export default async function middleware() {
+//   const m = await WebAssembly.instantiate(squareWasm)
+//   const answer = m.exports.square(9)
+ 
+//   const response = NextResponse.next()
+//   response.headers.set('x-square', answer.toString())
+//   return response
+// }
 
 export const config = {
 	runtime: 'edge',
